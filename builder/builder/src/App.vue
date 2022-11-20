ffe53f00544fc061a96512dc7faf94ae6ef25abd
Your Name <you@example.com>
2022-11-20 23:22:45 +0300

    "Вс 20 ноя 2022 23:22:45" -

---
+++ builder/builder/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app_builder">s</div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style>
+* {
+    margin: 0px;
+    padding: 0px;
+}
+#app_builder {
+    background: #000;
+    width: 3080px;
+    height: 1920px;
+}
+</style>
