f83fc74a91f0efa3fc33ae6e1868be11725566a4
Your Name <you@example.com>
2022-11-27 00:49:16 +0300

    "Вс 27 ноя 2022 00:49:16" -

--- builder/auto_install.py
+++ builder/auto_install.py
@@ -14,17 +14,21 @@ setting = dict(
     download_link='https://www.python.org/downloads/'
 )
 
+path_app = Path(__file__).parent.resolve()
+path_server = path_app / 'server'
+path_env = path_server / 'venv'
+venv_obj = venv.EnvBuilder(with_pip=True, upgrade_deps=True)
+venv_context = venv_obj.ensure_directories(str(path_env))
+path_python = venv_context.env_exec_cmd
+
 
 # 1. Проверить версию текущего python
 def step1(path_app: Path):
     python_version = sys.version_info
     if python_version.major >= setting['major'] and python_version.minor >= setting['minor']:
-        path_server = path_app / 'server'
-        path_env = path_server / 'venv'
-        path_python = step2(path_env=path_env)
+        step2(path_env=path_env)
         step3(
             path_python=path_python,
-            path_pyproject=path_server,
             path_server=path_server
         )
         step4(
@@ -43,14 +47,11 @@ def step1(path_app: Path):
 # 2. Создать виртуальное окружение
 def step2(path_env: Path):
     # Создаем виртуально окружение в папке `./venv`
-    v = venv.EnvBuilder(with_pip=True, upgrade_deps=True)
-    v.create(str(path_env))
-    context = v.ensure_directories(str(path_env))
-    return context.env_exec_cmd
+    venv_obj.create(str(path_env))
 
 
 # 3. Установить зависимости из файла `./pyproject.toml` в ВО
-def step3(path_python: str, path_pyproject: Path, path_server: Path):
+def step3(path_python: str, path_server: Path):
     # Установка `poetry`
     subprocess.check_call([path_python, '-m', 'pip', 'install', 'poetry'])
     # Обновить файлы блокировки
@@ -110,7 +111,60 @@ server/plagins
 *.sqlite
     """)
 
+# 7. Создать файл для автоматического обновления
+
+
+def step7(path_auto_update: Path):
+    path_auto_update.write_text('''
+""" 
+Автоматическое обновление программы
+"""
+
+import subprocess
+from datetime import datetime
+from auto_install import step3, path_server, path_python
+
+
+def syncGit():
+    """Синхронизация проекта"""
+    origin = 'origin'
+    # Получить имя текущей ветки, оно будет считаться именем для удаленной ветки.
+    select_branch: str = subprocess.run(
+        'git --no-pager branch --no-color --show-current', shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8'
+    ).stdout
+    if not select_branch:
+        raise ValueError("Пустая ветка")
+    # Проверить синхронизацию текущего локального проекта(даже если изменения небыли за комичены), с удаленной веткой.
+    diff: str = subprocess.run(
+        f'git --no-pager diff {origin}/{select_branch} --raw --name-status', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8'
+    ).stdout
+    if diff:
+        #
+        # Если есть различия в локальной и удаленной ветки
+        #
+        # Делаем коммит текущих локальных изменений.
+        subprocess.run('git add -A', check=True)
+        subprocess.run(
+            f"git commit -m 'CommitByAutoUpdate:{datetime.now()}'", check=True)
+        # Получаем всю информацию об изменениях на удаленной ветки.
+        subprocess.run('git fetch --all', shell=True, check=True)
+        # Принудительно(во всех спорных случая берем данные из удаленной ветки) синхронизируем локальную ветку с удаленной.
+        subprocess.run(
+            f'git reset --hard {origin}/{select_branch}', shell=True)
+        # Выполняем синхронизацию зависимостей в виртуальном окружение `Python`
+        syncPyVenvDependents()
+    else:
+        # Нет различий локальной ветки от удаленной. Или не удалось узнать различий с удаленной веткой, из за отсутствия связи.
+        ...
+
+
+def syncPyVenvDependents():
+    """Синхронизация зависимостей для виртуального окружения Python"""
+    step3(path_python=path_python, path_server=path_server)
+
+    ''')
+
 
 if __name__ == '__main__':
-    path_app = Path(__file__).parent.resolve()
     step1(path_app)
