64da93d92ce38281163a9b4c5a7dea02a3e48fb0
Your Name <you@example.com>
2022-11-27 12:17:54 +0300

    "Вс 27 ноя 2022 12:17:54" -

--- builder/auto_install.py
+++ builder/auto_install.py
@@ -137,36 +137,37 @@ def syncGit():
     select_branch: str = subprocess.run(
         'git --no-pager branch --no-color --show-current', shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8'
-    ).stdout
+    ).stdout.replace('\n', '')
     if not select_branch:
         raise ValueError("Пустая ветка")
     # Проверить синхронизацию текущего локального проекта(даже если изменения небыли за комичены), с удаленной веткой.
     diff: str = subprocess.run(
         f'git --no-pager diff {origin}/{select_branch} --raw --name-status', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8'
-    ).stdout
+    ).stdout.replace('\n', '')
     if diff:
         #
         # Если есть различия в локальной и удаленной ветки
         #
         # Делаем коммит текущих локальных изменений.
-        subprocess.run('git add -A', check=True)
+        subprocess.run('git add -A', shell=True, check=True)
         subprocess.run(
-            f"git commit -m 'CommitByAutoUpdate:{datetime.now()}'", check=True)
+            f"git commit -m 'CommitByAutoUpdate:{datetime.now()}'", shell=True,check=True)
         # Получаем всю информацию об изменениях на удаленной ветки.
         subprocess.run('git fetch --all', shell=True, check=True)
         # Принудительно(во всех спорных случая берем данные из удаленной ветки) синхронизируем локальную ветку с удаленной.
         subprocess.run(
             f'git reset --hard {origin}/{select_branch}', shell=True)
+        print('GitReset')
         # Выполняем синхронизацию зависимостей в виртуальном окружение `Python`
         syncPyVenvDependents()
     else:
         # Нет различий локальной ветки от удаленной. Или не удалось узнать различий с удаленной веткой, из за отсутствия связи.
         ...
 
-
 def syncPyVenvDependents():
     """Синхронизация зависимостей для виртуального окружения Python"""
     step3(path_python=path_python, path_server=path_server)
+
     ''')
 
 
